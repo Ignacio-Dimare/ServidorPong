69c9b75d8f305a1a539da70ebd95bdbbda054bf2
Ignacio <ignaciodim45@gmail.com>
2025-12-01 21:43:28 -0300

    keepalive

--- Servidor.py
+++ Servidor.py
@@ -1,4 +1,6 @@
 import socket
+import signal
+import os
 
 class TCPServer:
 
@@ -17,6 +19,42 @@ class TCPServer:
         while True:
             conn , addr = self.server_socket.accept()
             print(f"Conexion recibida de: {addr}")
+
+            pid = os.fork()
+
+            if pid == 0:
+                while True:
+                    print("Itera")
+
+            if os.fork() == 0:
+                while True:
+                    # Armo el select
+                    rlist, _, _ = select.select([conn], [], [], KEEPALIVE_TIMEOUT)
+
+                    if not rlist:
+                        # No llegaron datos, salio por timeout
+                        print(f"Timeout de keepalive para {addr}. Cerrando conexion...")
+                        self.server_socket.close()
+                        os.kill(pid, signal.SIGKILL)
+                        break
+
+                    try:
+                        data = conn.recv(BUFFER_SIZE)
+
+                        if not data:
+                            print(f"Cliente {addr} cerro la conexion.")
+                            break
+
+                        mensaje = data.decode()
+                        print(f"{addr}: {mensaje}")
+                        conn.sendall(b"OK")
+
+                    except ConnectionResetError:
+                        print(f"Cliente {addr} reseteo la conexion.")
+                        self.server_socket.close()
+                        os.kill(pid, signal.SIGKILL)
+                        break
+
 
     def keep_alive(self):
 
--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import socket
 
 HOST = "127.0.0.1"
 PORT = 5000
+
+from server import TCPServer
+
+def main():
+    server = TCPServer(
+        host = HOST,
+        port = PORT,
+        keepalive_timeout = 10,
+        buffer_size = 1024
+    )
+
+    server.start()
+
+if __name__ == "__main__":
+    main()
